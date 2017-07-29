e7066c527b8b3678077528cbdba425ecac3cbdef
Shekhar <Chandrashekhar.Thalkar@infinitylabs.in>
2017-07-29 12:14:59 +0530

    Solve ch01 ex 02 question basg version

---
+++ programming/languages/scripting/shell/bash/code/ch01_ex_02_bash_version.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Bash-Beginners-Guide 
+# Chapter 01 
+# 1.7. Exercises
+# Question 2
+clear
+printf "Q : Use the --version option to find out which version you are running?\n"
+printf "A : bash  --version\n"
+bash --version
