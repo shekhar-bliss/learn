42564fb6f24a231eaeddf2cb20aef5dbf4078b47
Shekhar <Chandrashekhar.Thalkar@infinitylabs.in>
2017-08-16 22:21:23 +0530

    3.8. Exercises 1 solution

---
+++ programming/languages/scripting/shell/bash/code/ch03_exe_01_variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Bash-Beginners-Guide
+# Chapter 03
+# 3.8. Exercises
+#
+#
+# Create 3 variables, VAR1, VAR2 and VAR3; initialize them to hold the values 
+# "thirteen", "13" and "Happy Birthday" respectively.
+clear 													# clear terminal window
+echo "Create 3 variables, VAR1, VAR2 and VAR3; initialize them to hold the values"
+echo "\"thirteen\", \"13\" and \"Happy Birthday\" respectively."
+echo 
+
+# set a local shell variable VAR1 and assign value
+VAR1="thirteen"
+# set a local shell variable VAR2 and assign value
+VAR2="13"
+# set a local shell variable VAR3 and assign value
+VAR3="Happy Birthday"
